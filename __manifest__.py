0a9bbf80b4c4aade0f43be6b94b7ba0b5666a588
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2022-11-29 22:15:20 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Akretion - Florian Mounier
+{
+    "name": "POS Receipt Client",
+    "summary": "Add Client to receipt.",
+    "author": "silvau",
+    "website": "http://zeval.com.mx",
+    "category": "Point of Sale",
+    "version": "14.0.1.0.1",
+    "license": "LGPL-3",
+    "depends": [
+        "point_of_sale",
+        "pos_restaurant"
+        ],
+    "qweb": [
+        "static/src/xml/OrderReceipt.xml",
+    ],
+}
